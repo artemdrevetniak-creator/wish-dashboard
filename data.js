f4dd1274fe19cab5ff7baae626e1d55a306bf4d8
artemdrevetniak-creator <artemdrevetniakgoit@gmail.com>
2026-07-04 19:45:38 +0300

    Відредагував дані

---
+++ data.js
@@ -0,0 +1,35 @@
+const user = {
+	name: 'Артем',
+	surname: 'Древетняк',
+	userPhoto: 'my-photo.jpg',
+	qrQode: 'qr-code.svg',
+	dreams: [
+		{ src: 'aston_martin.webp', alt: 'Авто моєї мрії' },
+		{ src: 'home.jpg', alt: 'Домівка на місяці' },
+		{ src: 'voyage.jpg', alt: 'Подорож до Марсу' },
+	],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+	500, 700, 800, 1000, 1200, 1500, 1800, 2100, 2400, 2700, 3000, 9300,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+	{ month: 'Червень', skill: 'JavaScript' },
+	{ month: 'Липень', skill: 'Java' },
+	{ month: 'Серпень', skill: 'Python' },
+	{ month: 'Вересень', skill: 'Самостійність' },
+	{ month: 'Жовтень', skill: 'Гнучкість' },
+	{ month: 'Листопад', skill: 'Емоційний інтелект' },
+	{ month: 'Грудень', skill: 'Креативність' },
+	{ month: 'Січень', skill: 'Лідерство' },
+	{ month: 'Лютий', skill: 'Нетворкінг' },
+	{ month: 'Березень', skill: 'Особистий бренд' },
+	{ month: 'Квітень', skill: 'Управління фінансами' },
+	{ month: 'Травень', skill: 'Публічні виступи' },
+];
+
+export { arr, necessaryExpenses, todo, user };
